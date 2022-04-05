6ce64b2956e474b1a9d8572970d655b1653e01f3
Jim97M <wabuyajames@gmail.com>
2022-04-05 20:31:57 +0300

    Dependancies

--- middlewares/profileImageMiddleware.js
+++ middlewares/profileImageMiddleware.js
@@ -1,1 +1,16 @@
 const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function(req, res, cb) {
+        cb(null, 'assets/');
+    },
+
+    filename: function(req, file, cb){
+        cb(null, Date.now() +'-'+  file.originalname);
+    }
+});
+
+exports.upload = multer({
+    storage: storage
+});
+
