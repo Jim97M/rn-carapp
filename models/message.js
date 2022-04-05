92d2aebb03bd8b366b262026d11ba479e31ddb79
Jim97M <wabuyajames@gmail.com>
2022-04-05 20:27:36 +0300

    Message Room Fields

---
+++ models/message.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const MessageSchema = new Schema(
+    {
+        id: Schema.ObjectId,
+        author: {type: Schema.Types.ObjectId, ref: "room", required: true},
+        room: {type: Schema.Types.ObjectId, ref: "room", required: true},
+        text: {type: String, required: true},
+        read: {type: Boolean, default: false},
+        attachments: [{type: String, required: true}]
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("message", MessageSchema);
