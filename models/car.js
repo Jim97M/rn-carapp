11eb369bda5501e10e4600ae0aba42b9b842f97a
Jim97M <wabuyajames@gmail.com>
2022-04-24 08:50:05 +0300

    Backend Fixes

--- models/car.js
+++ models/car.js
@@ -4,8 +4,9 @@ const Schema = mongoose.Schema;
 
 const CarSchema =new Schema({
     id: Schema.ObjectId,
-    make: {type: String, required: true},
-    model: {type: String, required: true}
+    name: {type: String, required: true},
+    model: {type: String, required: true},
+    color: {type: String, required: true}
 });
 
 module.exports = mongoose.model("car", CarSchema);
